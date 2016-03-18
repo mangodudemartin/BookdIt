40e6e9c878d1d76a27c4b11940763e0768c582f6
Mangodude <mangodudemartin@gmail.com>
2016-03-17 22:48:43 -0700

    removed trailing bookdit from URL

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^bookdit/', include('bookdit.urls')),
+    url(r'^', include('bookdit.urls')),
     url(r'^admin/', admin.site.urls),
 ]
